62cce544eff2a9e0da488fdb934930465ef46af0
smiceli <micelisol@hotmail.com>
2022-05-08 19:25:44 -0300

    Se agregan funciones y mejoras generales

--- assets/js/ejercicio.js
+++ assets/js/ejercicio.js
@@ -1,51 +1,56 @@
+//Declaro variables globales
 let costoTotal = 0;
-const costoLibro1 = 1000;
-const costoLibro2 = 2000;
-const costoLibro3 = 500;
-const costoLibro4 = 3000;
+const costo1 = 1000;
+const costo2 = 2000;
+const costo3 = 500;
+const costo4 = 3000;
+const nombre1 = "El día que Nieztsche lloró";
+const nombre2 = "El tercer ojo";
+const nombre3 = "El sari rojo";
+const nombre4 = "Los tres nombres del lobo";
 
-let a = Number(prompt(`Agregar un libro a la compra:
+
+//Declaro funciones a utilizar
+function verMenu() {
+    return Number(prompt(`Agregue un item a la compra:
                 1-El día que Nieztsche lloró ($1000) 
                 2-El tercer ojo ($2000)
                 3-El sari rojo ($500)
                 4-Los tres nombres del lobo ($3000)
-                5-Finalizar compra`));
-
-while (a !== 5) {
-    if (a === 1 || a === 2 || a === 3 || a === 4) {
-        let cantidad = (prompt(`Indique la cantidad del libro seleccionado`));
-        if (a === 1) {
-            costoTotal = costoTotal + (costoLibro1 * cantidad)
-            console.log("Acabas de agregar a la compra", cantidad, "libro/s El día que Nieztsche lloró")
-        } else if (a === 2) {
-            costoTotal = costoTotal + (costoLibro2 * cantidad)
-            console.log("Acabas de agregar a la compra", cantidad, "libro/s El tercer ojo")
-        } else if (a === 3) {
-            costoTotal = costoTotal + (costoLibro3 * cantidad)
-            console.log("Acabas de agregar a la compra", cantidad, "libro/s El sari rojo")
-        } else {
-            costoTotal = costoTotal + (costoLibro4 * cantidad)
-            console.log("Acabas de agregar a la compra", cantidad, "libro/s Los tres nombres del lobo")
-        }
-
-        console.log("Costo acumulado compra: $", costoTotal)
-
-        a = Number(prompt(`Agregar otro libro a la compra:
-                    1-El día que Nieztsche lloró ($1000) 
-                    2-El tercer ojo ($2000)
-                    3-El sari rojo ($500)
-                    4-Los tres nombres del lobo ($3000)
-                    5-Finalizar compra`));
-
-    } else {
-        alert("Opcion invalida. Debe seleccionar 1, 2, 3, 4 o 5");
-        a = Number(prompt(`Agregar otro libro a la compra:
-                    1-El día que Nieztsche lloró ($1000) 
-                    2-El tercer ojo ($2000)
-                    3-El sari rojo ($500)
-                    4-Los tres nombres del lobo ($3000)
-                    5-Finalizar compra`));
-    }
+                5-Finalizar compra
+                6-Cancelar compra`));
+}
+function calcularCosto(costoAcumulado, costoIndividual, nombreItem) {
+    let cantidad = Number(prompt(`Indique la cantidad del libro seleccionado`))
+    costoAcumulado = costoAcumulado + (costoIndividual * cantidad)
+    console.log("Acabas de agregar a la compra ", cantidad, "libro/s ", nombreItem)
+    console.log("Costo acumulado: $", costoAcumulado)
+    return costoAcumulado
 }
 
-console.log("Costo final compra: $", costoTotal)
+//Inicio parte principal del programa
+let item = verMenu();
+while ((item !== 5) && (item !== 6)) {
+    if (item === 1 || item === 2 || item === 3 || item === 4) {
+        if (item === 1) {
+            costoTotal = calcularCosto(costoTotal, costo1, nombre1)
+        } else if (item === 2) {
+            costoTotal = calcularCosto(costoTotal, costo2, nombre2)
+        } else if (item === 3) {
+            costoTotal = calcularCosto(costoTotal, costo3, nombre3)
+        } else {
+            costoTotal = calcularCosto(costoTotal, costo4, nombre4)
+        }
+    } else {
+        alert("Opcion invalida. Debe seleccionar 1, 2, 3, 4, 5 o 6");
+    }
+    item = verMenu();
+}
+if (item === 5) {
+    console.log("Costo final: $", costoTotal)
+}
+else {
+    costoTotal=0
+    console.log("Compra cancelada. Costo final: $", costoTotal)
+}
+console.log("Gracias por su interes")
